03caad592d8d72c8d419ea02dfe6913000d9eae2
Sushant Dhimal <sushant07.dhimal@gmail.com>
2022-11-08 23:23:45 +0545

    feat: add mui datagrid to show product details

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,169 +1,202 @@
-import {
-  Box,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Container,
-  Stack,
-  Typography,
-} from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
-import React, { useEffect } from "react";
+import { Avatar, Box, Button, Stack, Typography } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
+// rows of datagrid
+let product = [];
+
 const Cart = () => {
+  const [selectionModel, setSelectionModel] = useState([]);
+  const [rows, setRows] = useState(product);
+
   const productInCart = useSelector((state) => state.productList.productInCart);
 
-  useEffect(() => {}, []);
+  // columns of datagrid
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      width: 10,
+      editable: false,
+      sortable: false,
+    },
+    {
+      field: "img",
+      headerName: "Img",
+      width: 50,
+      editable: false,
+      sortable: false,
+      renderCell: (params) => {
+        let imgSrc = params.value;
+        return (
+          <>
+            <Avatar src={imgSrc} />
+          </>
+        );
+      },
+    },
+    {
+      field: "name",
+      headerName: "Product Name",
+      width: 250,
+      editable: false,
+      sortable: false,
+    },
+    {
+      field: "desc",
+      headerName: "Description",
+      headerAlign: "center",
+      type: "number",
+      width: 300,
+      sortable: false,
+    },
+    {
+      field: "quantity",
+      headerName: "Quantity",
+      sortable: false,
+      width: 80,
+    },
+    {
+      field: "price",
+      headerName: "Rate",
+      width: 80,
+      editable: false,
+      sortable: true,
+    },
+    {
+      field: "total",
+      headerName: "Total",
+      width: 100,
+      editable: false,
+      sortable: true,
+    },
+    {
+      field: "delete",
+      headerName: "Delete",
+      sortable: false,
+      width: 90,
+      disableColumnMenu: true,
+      // renderHeader: () => {
+      //   return (
+      //     <IconButton
+      //       onClick={() => {
+      //         const selectedIDs = new Set(selectionModel);
+      //         // you can call an API to delete the selected IDs
+      //         // and get the latest results after the deletion
+      //         // then call setRows() to update the data locally here
+      //         setRows((r) => r.filter((x) => !selectedIDs.has(x.id)));
+      //       }}
+      //     >
+      //       <DeleteIcon />
+      //     </IconButton>
+      //   );
+      // },
+    },
+  ];
+
+  // rows of datagrid
+  product = productInCart.map((productDetails) => {
+    let fetchProduct = productDetails.fetchProduct;
+    let productQuantity = productDetails.productQuantity;
+    let newObj = {
+      id: fetchProduct.id,
+      img: fetchProduct.image,
+      name: fetchProduct.title,
+      desc: fetchProduct.description,
+      quantity: productQuantity,
+      price: fetchProduct.price,
+      total: productQuantity * fetchProduct.price,
+    };
+
+    return newObj;
+  });
+
   return (
-    <Container sx={{ maxWidth: "1300px", p: { xs: 1, sm: 3 } }}>
-      {productInCart.length > 0
-        ? productInCart.map((product) => (
-            <Card
-              sx={{
-                p: { xs: 2, sm: 6 },
-                boxShadow: 4,
-                mt: { xs: 4, md: 6, xl: 10 },
-              }}
-              key={product.id}
-            >
-              <Stack
-                direction={{ md: "row", justifyContent: "center" }}
-                spacing={4}
-              >
-                <CardMedia
-                  component="img"
-                  height="300"
-                  image={product.image}
-                  alt=""
-                  sx={{
-                    // backgroundPosition: "top",
-                    objectFit: "contain",
-                    width: { xs: 1, sm: "250px" },
-                  }}
-                />
-                <Box>
-                  <CardContent sx={{ p: 0 }}>
-                    <Typography
-                      gutterBottom
-                      variant="h5"
-                      component="div"
-                      sx={{ pt: 2, fontWeight: 600 }}
-                    >
-                      {product.title}
-                    </Typography>
-                    <Typography variant="body1" color="text.secondary">
-                      {product.description}
-                    </Typography>
-                    <Typography
-                      // variant="h3"
-                      // color="text.secondary"
-                      sx={{
-                        fontSize: {
-                          xs: "2rem",
-                          md: "3rem",
-                        },
-                        pt: 2,
-                        color: "#f57c00",
-                      }}
-                    >
-                      ${product.price}
-                    </Typography>
-                  </CardContent>
-                  <Typography sx={{ fontWeight: 600 }}>Quantity:</Typography>
-                  <Stack
-                    direction="row"
-                    spacing={{ xs: 1, sm: 3 }}
-                    alignItems="center"
-                  >
-                    <Button
-                      sx={{
-                        width: "5px",
-                        height: "15px",
-                        py: 3,
-                        // px: 6,
-                        px: { xs: 4, sm: 6 },
-                        backgroundColor: "#eeeeee",
-                        "&:hover": { backgroundColor: "#e0e0e0" },
-                      }}
-                      // onClick={() => handleAddItemToStore(fetchProduct)}
-                      disabled={product.length === 10 ? true : false}
-                    >
-                      <AddIcon sx={{ color: "" }} />
-                    </Button>
-                    <Typography sx={{ fontSize: "40px" }}>
-                      {product.length}
-                    </Typography>
-                    <Button
-                      sx={{
-                        width: "5px",
-                        height: "15px",
-                        py: 3,
-                        px: { xs: 4, sm: 6 },
-                        backgroundColor: "#eeeeee",
-                        "&:hover": { backgroundColor: "#e0e0e0" },
-                      }}
-                      // onClick={() => handleRemoveItemFromStore(fetchProduct)}
-                      disabled={product.length === 0 ? true : false}
-                    >
-                      <RemoveIcon />
-                    </Button>
-                  </Stack>
-                  <CardActions
-                    sx={{
-                      px: 0,
-                      justifyContent: { xs: "center", sm: "flex-start" },
-                    }}
-                  >
-                    <Stack
-                      direction={{ xs: "column", sm: "row" }}
-                      mt={4}
-                      spacing={3}
-                    >
-                      {/* <TextField
-                    placeholder="Enter Voucher Code"
-                    sx={{}}
-                  ></TextField>
-                  <Button
-                    variant="contained"
-                    sx={{
-                      py: { xs: 1, sm: 2 },
-                      // py: 2,
-                      px: 6,
-                      backgroundColor: "#1565c0",
-                      "&:hover": { backgroundColor: "#2962ff" },
-                    }}
-                    // onClick={handleOpenLogIn}
-                  >
-                    apply
-                  </Button> */}
-                    </Stack>
-                  </CardActions>
-                  <Button
-                    size="small"
-                    variant="contained"
-                    sx={{
-                      py: { xs: 1, sm: 2 },
-                      // py: 2
-                      width: "100%",
-                      backgroundColor: "#f57c00",
-                      "&:hover": {
-                        backgroundColor: "#ff9800",
-                      },
-                    }}
-                    // onClick={handleOpenLogIn}
-                  >
-                    Process to checkout
-                  </Button>
-                </Box>
-              </Stack>
-            </Card>
-          ))
-        : "No product has been added to cart."}
-    </Container>
+    <>
+      <Box
+        sx={{
+          mt: 4,
+          px: 4,
+          position: "relative",
+          display: "flex",
+          gap: 4,
+          flexDirection: { xs: "column", md: "row" },
+        }}
+      >
+        <Stack
+          sx={{
+            maxWidth: "1600px",
+            width: { xs: "100%", lg: "75%" },
+            boxShadow: 2,
+          }}
+        >
+          {/*  */}
+          <Typography
+            variant="h1"
+            sx={{
+              fontSize: { xs: "20px", sm: "30px" },
+              fontWeight: 500,
+              color: "gray",
+              pt: 2,
+              p: 4,
+            }}
+          >
+            Shopping Items
+          </Typography>
+          <DataGrid
+            autoHeight
+            rows={rows}
+            columns={columns}
+            checkboxSelection
+            disableSelectionOnClick
+            disableColumnMenu
+            disableColumnSelector
+            pageSize={10}
+            rowsPerPageOptions={[5]}
+            rowCount={10}
+            experimentalFeatures={{ newEditingApi: true }}
+            onSelectionModelChange={(newSelectionModel) => {
+              setSelectionModel(newSelectionModel);
+            }}
+            selectionModel={selectionModel}
+            paginationMode="server"
+            keepNonExistentRowsSelected
+          />
+        </Stack>
+        <Box
+          sx={{
+            mr: 2,
+            width: "max-content",
+            minWidth: "200px",
+            height: "max-content",
+            boxShadow: 2,
+            p: 2,
+          }}
+        >
+          <Typography variant="h6" color="gray" sx={{ fontSize: "20px" }}>
+            No items selected
+          </Typography>
+          <Button
+            size="small"
+            variant="contained"
+            sx={{
+              py: 1,
+              px: 1,
+              width: "100%",
+              borderRadius: "20px",
+              textTransform: "none",
+              backgroundColor: "#f57c00",
+              "&:hover": {
+                backgroundColor: "#ff9800",
+              },
+            }}
+          >
+            Proceed to checkout
+          </Button>
+        </Box>
+      </Box>
+    </>
   );
 };
 
