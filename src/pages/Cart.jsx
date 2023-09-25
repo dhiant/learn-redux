f2cfbf0048315fd9040dfbd1cded8a180d5510fa
sushant dhimal <sushant07.dhimal@gmail.com>
2023-09-25 15:56:56 +0545

    feat(totalPrice): calculate total price of all cart items

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,24 +1,36 @@
 import React from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import { Container, Button, Stack, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 import ProductItemCard from "../components/ProductItemCard";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
 	const productsInCart = useSelector(
 		(state) => state.productList.productInCart
 	);
 
+	const totalPriceOfAllCartItems = productsInCart.reduce(
+		(acc, currentProduct) => {
+			let qty = currentProduct.productQuantity;
+			let rate = currentProduct.fetchProduct.price;
+			let price = qty * rate;
+			return price + acc;
+		},
+		0
+	);
+
 	return (
 		<>
-			<Box
+			<Container
 				sx={{
 					my: 4,
 					px: 4,
 				}}
+				maxWidth="xl"
 			>
 				<Stack
 					sx={{
-						maxWidth: "1700px",
+						// maxWidth: "1700px",
 						width: 1,
 						boxShadow: 2,
 						"& .quantity--cell": {
@@ -43,18 +55,74 @@ const Cart = () => {
 					</Typography>
 
 					{/* each cart items */}
-					{productsInCart.map((item, idx) => (
-						<ProductItemCard
-							idx={idx}
-							img={item.fetchProduct.image}
-							title={item.fetchProduct.title}
-							desc={item.fetchProduct.description}
-							rate={item.fetchProduct.price}
-							qty={item.productQuantity}
-						/>
-					))}
+					{productsInCart.length > 0 ? (
+						<>
+							{productsInCart.map((item, idx) => (
+								<ProductItemCard
+									idx={idx}
+									img={item.fetchProduct.image}
+									title={item.fetchProduct.title}
+									desc={item.fetchProduct.description}
+									rate={item.fetchProduct.price}
+									qty={item.productQuantity}
+								/>
+							))}
+							{/* total price of all items in cart */}
+							<Typography
+								sx={{
+									fontSize: {
+										xs: "20px",
+										md: "24px",
+									},
+									fontWeight: 600,
+									textAlign: "end",
+									pr: 2,
+								}}
+							>
+								Total:{" "}
+								<span
+									style={{
+										color: "#f57c00",
+										fontSize: {
+											xs: "1.5rem",
+											md: "2rem",
+										},
+									}}
+								>
+									${totalPriceOfAllCartItems.toFixed(2)}
+								</span>
+							</Typography>
+
+							<Button
+								variant="contained"
+								component={Link}
+								to="/checkout"
+								sx={{
+									py: "12px",
+									// maxWidth: "400px",
+									// margin: "auto",
+									mt: 2,
+								}}
+							>
+								Proceed to checkout
+							</Button>
+						</>
+					) : (
+						<Typography
+							variant="h2"
+							sx={{
+								fontSize: { xs: "1.5rem", xl: "2rem" },
+								fontWeight: "semi-bold",
+								color: "#000",
+								textAlign: "center",
+								pb: 2,
+							}}
+						>
+							Cart is empty.
+						</Typography>
+					)}
 				</Stack>
-			</Box>
+			</Container>
 		</>
 	);
 };
